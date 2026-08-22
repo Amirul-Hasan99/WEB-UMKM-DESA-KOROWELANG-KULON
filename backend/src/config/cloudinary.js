b4c01ed428dfd7ee8524bf11b74680af615b9023
hasanamirul <hasanamirul919@gmail.com>
2026-08-22 13:00:41 +0700

    feat: support both CLOUDINARY_URL and discrete environment variables

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,12 +1,18 @@
 const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
-});
+if (process.env.CLOUDINARY_URL) {
+  cloudinary.config({
+    secure: true,
+  });
+} else {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
+  });
+}
 
 /**
  * Upload buffer to Cloudinary and return secure URL.
